564a4aebdd7fd33d550fae02a1e3486f3736b947
Thanin <chea14chainat@gmail.com>
2024-11-28 15:04:30 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,37 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+.get("/", () => "Hello Elysia")
+.get("/hello", ()=> "hello world")
+.get("/hello/:name/:age", ({params})=> {
+  const name = params.name;
+  const age = params.age;
+  return `hello ${name} ${age}`;
+})
+
+.get("/customer", ({params})=> {
+  const customer = [
+    {id: 1, name: "John", age: 20},
+    {id: 2, name: "Jane", age: 21},
+    {id: 3, name: "Jim", age: 22},
+  ]
+  return customer;
+})
+.get("/customer/:id", ({params})=> {
+  const customers = [
+    {id: 1, name: "John", age: 20},
+    {id: 2, name: "Jane", age: 21},
+    {id: 3, name: "Jim", age: 22},
+  ]
+  const customer = customers.find((customer)=> customer.id === parseInt(params.id));
+  if(!customer) return {error: "Customer not found"};
+  return customer;
+})
+
+.listen(3000);
+
+//get params
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
